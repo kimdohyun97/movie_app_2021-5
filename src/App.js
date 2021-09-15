3a01ec9810ef5e1d5975b9739ce5d39752ca8d14
kimdohyun <ehgus5105@gmail.com>
2021-09-15 16:51:06 +0900

    image 넣기

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 
 const foodLike = [
   {
+    id: 1,
     name: "chicken" ,
     image: "https://health.chosun.com/site/data/img_dir/2021/03/31/2021033102448_0.jpg"
   },
   {
+    id: 2,
     name: "ham",
     image: "http://image.newdaily.co.kr/site/data/img/2017/07/10/2017071010070_0.jpg"
   }
 ]
 
-// function renderFood(dish) {
-//   return  (
-//     <Food name={dish.name} picture={dish.image} />
-//   )
-// }
-
-const renderFood = dish => <Food name={dish.name} picture={dish.image} />
 
 function App() {
   return (
     <div>
       {
-        foodLike.map(dish =>  (<Food name={dish.name} picture={dish.image} />))
+        foodLike.map(dish =>  (<Food key={dish.id} name={dish.name} picture={dish.image} />))
       }
     </div>
   );
@@ -32,7 +27,7 @@ function Food({name, picture}) {
   return (
     <div>
     <h1>I like {name} </h1>
-    <img src={picture} />
+    <img src={picture} alt={name}/>
  </div>
   )
 }
