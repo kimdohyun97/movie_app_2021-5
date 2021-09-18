211c60a72e88838f82cb310419fcd53c7ff01620
kimdohyun <ehgus5105@gmail.com>
2021-09-18 17:41:54 +0900

    App.js 수정
    Movie 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
 
-const foodLike = [
-  {
-    id: 1,
-    name: "chicken" ,
-    image: "https://health.chosun.com/site/data/img_dir/2021/03/31/2021033102448_0.jpg"
-  },
-  {
-    id: 2,
-    name: "ham",
-    image: "http://image.newdaily.co.kr/site/data/img/2017/07/10/2017071010070_0.jpg"
-  }
-]
-
-
 function App() {
   return (
     <div>
-      {
-        foodLike.map(dish =>  (<Food key={dish.id} name={dish.name} picture={dish.image} />))
-      }
+        <h1>Hello</h1>
+        <Movie />
     </div>
-  );
-}
-
-function Food({name, picture}) {
-  return (
-    <div>
-    <h1>I like {name} </h1>
-    <img src={picture} alt={name}/>
- </div>
   )
 }
+
+function Movie()
+return <h1>I like potato</h1>
 
 export default App;
