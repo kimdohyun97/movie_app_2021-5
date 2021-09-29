a9686d5f56a999fb41bf824394f8d7d2e1349519
kimdohyun <ehgus5105@gmail.com>
2021-09-29 16:03:16 +0900

    Edit App.js
    버튼추가

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ class App extends Component {
     }
     render() {
         return (
+            <div>
             <h1>The number is: {this.state.count} </h1>
+            <button>Add</button>
+            <button>Minus</button>
+            </div>
         )
     }
 }
