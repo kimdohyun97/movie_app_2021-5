9eb97d59736d73f72eb400db7c2c9ca054d56656
kimdohyun <ehgus5105@gmail.com>
2021-09-29 16:18:34 +0900

    Edit App.js
    버튼누를때마다 숫자 변경

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ class App extends Component {
     }
 
     add = () => {
-        this.setState({count: 1})
+        this.setState({count: this.state.count + 1})
     }
 
     minus = () => {
-        this.setState({count: -1})
+        this.setState({count: this.state.count + -1})
     }
 
     render() {
