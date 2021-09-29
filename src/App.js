29b4e135b60851a679620a15fafcdf10cfac63d9
kimdohyun <ehgus5105@gmail.com>
2021-09-29 15:46:38 +0900

    Edit App.js
    React를 {Component}로 변경

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react"
+import {Component} from "react"
 
-class App extends React.Component {
+class App extends Component {
     render() {
         return (
             <h1>Hello</h1>
