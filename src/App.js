c2fe3c0b4abaff028b14e418bff824f341e32fce
kimdohyun <ehgus5105@gmail.com>
2021-09-29 16:07:16 +0900

    Edit App.js
    버튼누를때마다 콘솔에 호출

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ class App extends Component {
     state = {
         count: 0,
     }
+
+    add = () => {
+        console.log('add')
+    }
+
+    minus = () => {
+        console.log('minus')
+    }
+
     render() {
         return (
             <div>
             <h1>The number is: {this.state.count} </h1>
-            <button>Add</button>
-            <button>Minus</button>
+            <button onClick={this.add}>Add</button>
+            <button onClick={this.minus}>Minus</button>
             </div>
         )
     }
