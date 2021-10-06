b331c2fa232ee62e6a96d335fb52766bfad0af03
kimdohyun <ehgus5105@gmail.com>
2021-10-06 14:29:22 +0900

    Edit App.js
    true -> 6초후 false

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ class App extends React.Component {
     state = {
         isLoading: true
     }
+
+    componentDidMount() {
+        setTimeout( () => {
+            this.setState({ isLoading: false })
+        } , 6000)
+    }
+
     render() {
         const { isLoading } = this.state
         return(
