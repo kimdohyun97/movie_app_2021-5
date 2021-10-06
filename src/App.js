0065e3a74d1714461f4ed6e993d7a69e43bf9b9c
kimdohyun <ehgus5105@gmail.com>
2021-10-06 14:18:26 +0900

    Edit App.js
    App.js 새로만들기

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
-import {Component} from "react"
+import React from "react"
 
-class App extends Component {
-    constructor(props) {
-        super(props)
-        console.log('constructor')
-    }
-    
-    componentDidMount() {
-        console.log('componentDidMount');
-    }
-    componentDidUpdate() {
-        console.log('componentDidUpdate...GoodBye');
-    }
-
-    state = {
-        count: 0,
-    }
-
-    add = () => {
-        this.setState({count: this.state.count + 1})
-    }
-
-    minus = () => {
-        this.setState({count: this.state.count + -1})
-    }
-
+class App extends React.Component {
+    state
     render() {
-        console.log('render')
-        return (
+        return(
             <div>
-            <h1>The number is: {this.state.count} </h1>
-            <button onClick={this.add}>Add</button>
-            <button onClick={this.minus}>Minus</button>
+                <h1>Hello!</h1>
             </div>
         )
     }
