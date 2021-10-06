cfb1f0cff8230476b21751662ff3be10888b1f60
kimdohyun <ehgus5105@gmail.com>
2021-10-06 14:23:40 +0900

    Edit App.js
    이름변경

---
+++ src/02-App.js
@@ -0,0 +1,36 @@
+import {Component} from "react"
+class App extends Component {
+    constructor(props) {
+        super(props)
+        console.log('constructor')
+    }
+    
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+    componentDidUpdate() {
+        console.log('componentDidUpdate');
+        console.log('componentDidUpdate...GoodBye');
+    }
+
+    state = {
+        count: 0,
+    }
+    add = () => {
+        this.setState({count: this.state.count + 1})
+    }
+    minus = () => {
+        this.setState({count: this.state.count + -1})
+    }
+    render() {
+        console.log('render')
+        return (
+            <div>
+            <h1>The number is: {this.state.count} </h1>
+            <button onClick={this.add}>Add</button>
+            <button onClick={this.minus}>Minus</button>
+            </div>
+        )
+    }
+}
+export default App
