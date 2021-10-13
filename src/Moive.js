7698bdf5f66993bd4bf49504dc572e84fabd3bb4
kimdohyun <ehgus5105@gmail.com>
2021-10-13 15:30:02 +0900

    Edit Movie.js propTypes 추가

--- src/Moive.js
+++ src/Moive.js
@@ -1,13 +1,18 @@
+import { getByTitle } from '@testing-library/react'
 import PropTypes from 'prop-types'
 
-function Movie() {
+function Movie({id, year, title, summary, poster}) {
     return (
-        <h1>Hello</h1>
+        <h4>{title}</h4>
     )
 }
 
-Moive.PropTypes = {
-
+Movie.propTypes = {
+   id: PropTypes.number.isRequired,
+   year: PropTypes.string.isRequired,
+   title: PropTypes.string.isRequired,
+   summary: PropTypes.string.isRequired,
+   poster: PropTypes.string.isRequired
 }
 
 export default Movie
