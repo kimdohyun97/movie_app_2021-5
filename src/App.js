ccc21d4b9247db173384607d56b99a29d0798bef
kimdohyun <ehgus5105@gmail.com>
2021-10-30 12:01:43 +0900

    Edit App.js
    내용다지우고
    import HashRouter, Route

--- src/App.js
+++ src/App.js
@@ -1,56 +1,12 @@
-import React from "react"
-import axios from "axios"
-import Movie from "./components/Moive"
 import "./App.css"
+import { HashRouter, Route } from 'react-router-dom'
 
-class App extends React.Component {
-    state = {
-        isLoading: true,
-        movies: []
-    }
-
-    getMovies = async () => {
-        const {
-            data: {
-                data: {movies}
-            }
-        } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating")
-        console.log(movies);
-        this.setState({movies, isLoading: false})
-    }
-
-    componentDidMount() {
-        this.getMovies()
-    }
-
-    render() {
-        const { isLoading, movies } = this.state
-        return(
-            <section className='container'>
-                { isLoading ? (
-                    <div className='loader'>
-                        <span className='loader-text'>Loading...</span>
-                    </div>
-                ) : (
-                  <div className='movies'>
-                     {
-                        movies.map((movie) => {
-                         return (
-                            <Movie 
-                            key = {movie.id}
-                            id = {movie.id}
-                            year = {movie.year}
-                            title = {movie.title}
-                            summary = {movie.summary}
-                            poster = {movie.medium_cover_image}
-                            genres = {movie.genres}
-                            /> 
-                         )})} 
-                  </div>    
-                )}
-                </section>
-                )
-            }
+function App() {
+    return (
+        <HashRouter>
+            <Route />
+        </HashRouter>
+    )
 }
 
 export default App
