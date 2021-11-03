b29a7706beb40b0f4444d0cba23f3d7e0d273a98
kimdohyun97 <ehgus5105@gmail.com>
2021-11-03 15:20:05 +0900

    Edit App.js
    import 네비게이션

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css"
 import { HashRouter, Route } from 'react-router-dom'
 import About from './routes/About'
 import Home from "./routes/Home"
+import Navigation from "./components/Navigation"
 
 function App() {
     return (
         <HashRouter>
+            <Navigation />
             <Route path="/" exact={true} component={Home} />
-            <Route path=' /about' component={About} />
+            <Route path='/about' component={About} />
         </HashRouter>
     )
 }
